f97a68a426c5862ca4787f1d3766ef6bb65252b3
Nicolas Charbonier <ncharbonnier@gmail.com>
2022-10-14 16:18:04 -0300

    Add some browsing capabilities, select bucket, and add next page button. Fix Uri for keys encoding links

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -21,6 +21,12 @@ namespace S3Browser.Pages
 
         public List<S3Object> Items { get; set; } = new List<S3Object>();
 
+        public string Token { get; set; } = string.Empty;
+
+        public string BucketName { get; set; } = string.Empty;
+
+        public string Prefix { get; set; } = string.Empty;
+
         public List<string> Buckets { get; set; } = new List<string>();
 
         public async Task<IActionResult> OnGet()
@@ -35,48 +41,27 @@ namespace S3Browser.Pages
             var s3Client = new AmazonS3Client(awsAccessId, awsAccessSecret);
 
             if (HttpContext.Request.QueryString.HasValue &&
-                !string.IsNullOrWhiteSpace(HttpContext.Request.Query["item"]))
-            {
-                var bucketName = HttpContext.Request.Query["bucket"];
-                var itemKey = HttpContext.Request.Query["item"];
-                GetObjectRequest requestDownload = new GetObjectRequest
-                {
-                    BucketName = bucketName,
-                    Key = itemKey
-                };
-                using (GetObjectResponse responseDownload = await s3Client.GetObjectAsync(requestDownload))
-                using (Stream responseStream = responseDownload.ResponseStream)
-                using (StreamReader reader = new StreamReader(responseStream))
-                {
-                    string title = responseDownload.Metadata["x-amz-meta-title"];
-                    string contentType = responseDownload.Headers["Content-Type"];
-
-
-                    //string responseBody = reader.ReadToEnd();
-                    return File(responseStream, contentType);
-                }
-            }
-            else if (HttpContext.Request.QueryString.HasValue &&
                 !string.IsNullOrWhiteSpace(HttpContext.Request.Query["bucket"]))
             {
                 // List bucket items
-                var bucketName = HttpContext.Request.Query["bucket"];
-                string token = null;
+                BucketName = HttpContext.Request.Query["bucket"];
+                Prefix = HttpContext.Request.Query["prefix"];
+                Token = HttpContext.Request.Query["token"];
                 ListObjectsV2Response response;
-                do
+                ListObjectsV2Request request = new ListObjectsV2Request();
+                request.BucketName = BucketName;
+                request.Prefix = Prefix;
+                request.ContinuationToken = Token;
+                response = await s3Client.ListObjectsV2Async(request);
+                foreach (var item in response.S3Objects)
                 {
-                    ListObjectsV2Request request = new ListObjectsV2Request();
-                    request.BucketName = bucketName;
-                    request.Prefix = HttpContext.Request.Query["prefix"];
-                    request.ContinuationToken = token;
-                    response = await s3Client.ListObjectsV2Async(request);
-                    foreach (var item in response.S3Objects)
-                    {
-                        if(item.Size > 0)
-                            Items.Add(item);
-                    }
-                    token = response.NextContinuationToken;
-                } while (response.IsTruncated == true);
+                    Items.Add(item);
+                }
+
+                if (response.IsTruncated)
+                {
+                    Token = response.NextContinuationToken;
+                }
                 return Page();
             }
             else
